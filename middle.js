aab84cb036c79dbe01a8b77c929f08619473ec9e
Jackson Stark <jacksonstark77@hotmail.com>
2019-09-18 01:24:57 +0000

    update

--- middle.js
+++ middle.js
@@ -40,10 +40,10 @@ const middle = (array) => {
 
 
 // TEST CODE
-assertArraysEqual(middle([1]),[])
-assertArraysEqual(middle([1, 2]) ,[])
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4])
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1, 2]) ,[]);
+assertArraysEqual(middle([1, 2, 3]),[2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
 

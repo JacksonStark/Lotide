bfcda0232cca1774ba9565f1bb47e633e1377426
Jackson Stark <jacksonstark77@hotmail.com>
2019-09-22 23:00:32 +0000

    update

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,6 @@
 // ASSERT EQUAL...
 
 const eqArrays = (array1, array2) => {
-
   // Initial check to see if lengths match...
   if (array1.length !== array2.length) {
     return false;
@@ -17,35 +16,33 @@ const eqArrays = (array1, array2) => {
 };
 
 const eqObjects = (objA, objB) => {
-
   // Extracting both objects' keys...
   let objAKeys = Object.keys(objA);
   let objBKeys = Object.keys(objB);
-
 
   // Initial check to see if keys lengths match...
   if (objAKeys.length !== objBKeys.length) {
     return false;
   } else {
-
     // Iteration over keys...
     for (const key of objAKeys) {
-
       // Checking if they any keys are arrays...
       if (Array.isArray(objA[key]) && Array.isArray(objB[key])) {
-
         // If they are both arrays...eqArrays is used to check for a falsy value...
         if (eqArrays(objA[key], objB[key]) === false) {
           return false;
         }
 
         // Checking if each key's value is an object...then using recursion...
-      } else if (typeof objA[key] === 'object' && typeof objB[key] === 'object') {
+      } else if (
+        typeof objA[key] === "object" &&
+        typeof objB[key] === "object"
+      ) {
         const result = eqObjects(objA[key], objB[key]);
         if (!result) {
           return false;
         }
-        
+
         // Checking if each key's value is a match...
       } else if (objA[key] !== objB[key]) {
         return false;
@@ -56,14 +53,17 @@ const eqObjects = (objA, objB) => {
 };
 
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
+  const inspect = require("util").inspect;
   if (eqObjects(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(
+      `🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
   }
 };
 
-
 // TEST CODE
-assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' });
+assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" });
